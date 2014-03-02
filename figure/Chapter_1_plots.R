9ed449ea4027de898b434de7da73a3114e56cf95
Lluis Ramon <lluisramon.tgn@gmail.com>
2014-03-02 18:13:35 +0100

    Update two binomial likelihood graphs.

--- figure/Chapter_1_plots.R
+++ figure/Chapter_1_plots.R
@@ -5,9 +5,24 @@ plot.new()
 text(0.5, 0.5, "Missing plot", cex = 1.5)
 dev.off()
 
-# Session 4
+# Session 2
+
+lbinom <- function(theta, size = 10, succes = 6){
+  
+  choose(size, succes)*(theta^succes)*(1-theta)^(size - succes)
+  
+}
 
 x <- seq(0, 1, by = 0.01)
+
+png(filename = "figure/likelihood_binomial.png")
+plot(x, lbinom(x, 10, 6), type = 'l', xlab= '', ylab= '')
+abline(v = 0.6, col = 'red')
+dev.off()
+
+# Session 4
+
+
 
 png(filename = "figure/beta_prior_pos_50_50.png")
 plot(dbeta(x, 56, 54), col = "red", type = "l")
